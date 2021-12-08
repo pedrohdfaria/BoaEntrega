25212139f156eab5cd2349671d393de1f8a0bb91
Pedro Henrique Dornellas Faria <faria.pedrohd@gmail.com>
2021-12-08 10:06:53 -0300

    Adicionar arquivos de projeto.

---
+++ POC/Controllers/Models/Route.cs
@@ -0,0 +1,11 @@
+﻿namespace POC.Controllers.Models
+{
+    public class Route
+    {
+        public  Coordinate Starting {get; set;}
+        public  Coordinate Destination {get; set;}
+        public string[] Directions { get; set; }
+    }
+
+    
+}
---
+++ POC/Controllers/RoutesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using POC.Controllers.Models;
+
+namespace POC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoutesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] Route route)
+        {
+            return "Route Created";
+        }
+
+    }
+}
